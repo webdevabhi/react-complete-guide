ee7c49f42b1e53c7284742d29c05381c84f56e2b
webdevabhi <abhishek.patel185@gmail.com>
2018-05-25 18:21:20 +0530

    Dymic data passing to components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const person = (props) => {
-    return <p>I am {props.name} and I am {props.age} years old.</p>
+    return (
+        <div>
+            <p>I am {props.name} and I am {props.age} years old.</p>
+            <pre>{props.children}</pre>
+        </div>
+    )
 }
 
 export default person;

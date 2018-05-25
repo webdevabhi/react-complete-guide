5e48e53065628259a6c778aaca9f5e1bab9d800d
webdevabhi <abhishek.patel185@gmail.com>
2018-05-25 11:08:48 +0530

    Another way to render html

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ class App extends Component {
         <h1>Hi, I am React App.</h1>
       </div>
     );
+
+    /*
+     * Another JS way to render html using pure ReactJs.
+     * Its Better to use Jsx, as it is simple and easy to understand.
+     */
+    // return React.createElement('div', {className:'App'}, React.createElement('h1', null, 'Hi, I am React App.'));
   }
 }
 

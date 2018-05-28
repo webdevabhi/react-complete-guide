727c88c2fe8b8199b026a690277968cd7f6b9c88
webdevabhi <abhishek.patel185@gmail.com>
2018-05-28 14:47:23 +0530

    Styling the component

--- src/App.js
+++ src/App.js
@@ -38,11 +38,22 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am React App.</h1>
         <p>I am redendering from App Root. {this.state.otherProperty}</p>
-        <button onClick={() => this.switchNameHandler('Abhishek Patel!')}>Switch Name</button>
+        <button 
+          style={style} 
+          onClick={() => this.switchNameHandler('Abhishek Patel!')}
+        >Switch Name</button>
 
         <Person 
           name={this.state.persons[0].name} 

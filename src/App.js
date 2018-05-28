870950fd640cb3b655ace7eba6f52ec5b2bd4e4e
webdevabhi <abhishek.patel185@gmail.com>
2018-05-28 12:54:49 +0530

    updating state

--- src/App.js
+++ src/App.js
@@ -8,18 +8,30 @@ class App extends Component {
       { name: 'Abhishek', age: 24 },
       { name: 'Sumit', age: 27 },
       { name: 'Yash', age: 19 }
-    ]
+    ],
+    otherProperty : "Some Other variable"
   }
 
   switchNameHandler = () => {
-    alert("Button CLicked");
+    // console.log("Button CLicked");
+
+    // Don't do this
+    // this.state.persons[0].name = 'Abhishek Patel';
+
+    this.setState({
+      persons: [
+        { name: 'Abhishek Patel', age: 24 },
+        { name: 'Sumit', age: 27 },
+        { name: 'Yash', age: 22 }
+      ]
+    })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I am React App.</h1>
-        <p>I am redendering from App Root.</p>
+        <p>I am redendering from App Root. {this.state.otherProperty}</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >

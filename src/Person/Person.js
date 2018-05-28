1b3c8bd15d248040c2c65a7f2f413228ea4ac095
webdevabhi <abhishek.patel185@gmail.com>
2018-05-28 14:37:12 +0530

    basic two way binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,8 @@ const person = (props) => {
         <div>
             <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
             {props.children}
+
+            <input type="tex" onChange={props.changed} value={props.name}/>
         </div>
     )
 }

70de3b970309fb57701b54493dd63f5a7f74f3e6
webdevabhi <abhishek.patel185@gmail.com>
2018-05-29 12:51:06 +0530

    started uning radium for sudo class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -52,8 +53,12 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
-    }
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
+    };
 
     let persons = null;
     if (this.state.showPerson) {
@@ -73,6 +78,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = [];
@@ -104,4 +113,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);

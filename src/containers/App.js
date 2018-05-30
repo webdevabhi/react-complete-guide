b280bda3925afc71c5ee7770f308e91c6522675d
webdevabhi <abhishek.patel185@gmail.com>
2018-05-30 17:14:57 +0530

    using setStae correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends PureComponent {
         { id: 3, name: 'Yash', age: 19 }
       ],
       otherProperty: "Some Other variable",
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0,
     }
   }
 
@@ -82,7 +83,18 @@ class App extends PureComponent {
 
   togglePersonhandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+
+    /* 
+     * better approach to set state
+     * this.setState({showPersons: !doesShow}); 
+    */
+
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   }
 
   render() {

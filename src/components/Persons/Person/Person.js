e6748591fa31f5813acb32d0c460f1c55074df89
webdevabhi <abhishek.patel185@gmail.com>
2018-05-30 11:57:33 +0530

    Converted stateless comp to statefull comp

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Person.css';
 
-const person = (props) => {
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
-            {props.children}
+class Person extends Component {
+    render() {
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old.</p>
+                {this.props.children}
 
-            <input type="tex" onChange={props.changed} value={props.name}/>
-        </div>
-    )
-}
+                <input type="tex" onChange={this.props.changed} value={this.props.name} />
+            </div>
+        );
+    }
+};
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person
-        name={person.name}
-        age={person.age}
-        click={() => props.clicked(index)}
-        changed={(e) => props.changed(e, person.id)}
-        key={person.id} />
-});
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.props.clicked(index)}
+                changed={(e) => this.props.changed(e, person.id)}
+                key={person.id} />
+        });
+    }
+};
 
-export default persons;
+export default Persons;

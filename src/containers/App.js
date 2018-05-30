340d502092d14810e33305ca9e33cae5a9866b65
webdevabhi <abhishek.patel185@gmail.com>
2018-05-30 16:14:22 +0530

    Custom High order component

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends PureComponent {
   constructor(props) {
@@ -95,7 +96,7 @@ class App extends PureComponent {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => { this.setState({ showPersons: true }) }}>Show Person</button>
         <Cockpit
           appTitle={this.props.title}
@@ -104,7 +105,7 @@ class App extends PureComponent {
           clicked={this.togglePersonhandler} />
 
         {persons}
-      </div>
+      </WithClass>
     );
 
     /*

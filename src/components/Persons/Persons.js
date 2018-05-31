4f648698ab36c8714124e0da02e7c545399a8386
webdevabhi <abhishek.patel185@gmail.com>
2018-05-31 10:45:19 +0530

    React Context Api to pass variables globally.

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -4,48 +4,47 @@ import Person from './Person/Person';
 class Persons extends PureComponent {
   constructor(props) {
     super(props);
-    // console.log('[Persons.js] Inside Constructor', props);
+    console.log('[Persons.js] Inside Constructor', props);
     this.lastPersonRef = React.createRef();
   }
 
   componentWillMount() {
-    // console.log('[Persons.js] Inside componentWillMount');
+    console.log('[Persons.js] Inside componentWillMount');
   }
 
   componentDidMount() {
-    // console.log('[Persons.js] Inside componentDidMount');
+    console.log('[Persons.js] Inside componentDidMount');
     this.lastPersonRef.current.focus();
   }
 
   componentWillReceiveProps(nextProps) {
-    // console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
+    console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
   }
 
   // shouldComponentUpdate(nextProps, nextState) {
-  //   // console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
   //   return nextProps.persons !== this.props.persons ||
   //     nextProps.changed !== this.props.changed ||
   //     nextProps.clicked !== this.props.clicked;
   // }
 
   componentWillUpdate(nextProps, nextState) {
-    // console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
+    console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
   }
 
   componentDidUpdate() {
-    // console.log('[UPDATE Persons.js] Insite componentDitUpdate');
+    console.log('[UPDATE Persons.js] Insite componentDitUpdate');
   }
 
   render() {
-    // console.log('[Persons.js] Inside render');
+    console.log('[Persons.js] Inside render');
     return this.props.persons.map((person, index) => {
       return <Person
         name={person.name}
         age={person.age}
         click={() => this.props.clicked(index)} 
         position={index} 
-        ref={this.lastPersonRef} 
-        authenticated={this.props.isAuthenticated}
+        ref={this.lastPersonRef}
         changed={(e) => this.props.changed(e, person.id)}
         key={person.id} />
     });
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,10 +5,12 @@ import Cockpit from '../components/Cockpit/Cockpit';
 import Aux from '../hoc/Aux';
 import withClass from '../hoc/withClass';
 
+export const AuthContext = React.createContext(false);
+
 class App extends PureComponent {
   constructor(props) {
     super(props);
-    // console.log('[App.js] Inside Constructor', props);
+    console.log('[App.js] Inside Constructor', props);
     this.state = {
       persons: [
         { id: 1, name: 'Abhishek', age: 24 },
@@ -23,25 +25,25 @@ class App extends PureComponent {
   }
 
   componentWillMount() {
-    // console.log('[App.js] Inside componentWillMount');
+    console.log('[App.js] Inside componentWillMount');
   }
 
   componentDidMount() {
-    // console.log('[App.js] Inside componentDidMount');
+    console.log('[App.js] Inside componentDidMount');
   }
 
   // shouldComponentUpdate(nextProps, nextState) {
-  //   // console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+  //   console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
   //   return nextState.persons !== this.state.persons ||
   //     nextState.showPersons !== this.state.showPersons;
   // }
 
   componentWillUpdate(nextProps, nextState) {
-    // console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
   }
 
   componentDidUpdate() {
-    // console.log('[UPDATE App.js] Insite componentDitUpdate');
+    console.log('[UPDATE App.js] Insite componentDitUpdate');
   }
 
   // If contructor are there better to initialise there.
@@ -103,14 +105,13 @@ class App extends PureComponent {
   }
 
   render() {
-    // console.log('[App.js] Inside render');
+    console.log('[App.js] Inside render');
     let persons = null;
 
     if (this.state.showPersons) {
       persons = <Persons 
         persons={this.state.persons} 
-        clicked={this.deletePersonHandler} 
-        isAuthenticated={this.state.authenticated}
+        clicked={this.deletePersonHandler}
         changed={this.nameChangedHandler} />;
     }
 
@@ -124,7 +125,7 @@ class App extends PureComponent {
           login={this.authenticationHandler}
           clicked={this.togglePersonhandler} />
 
-        {persons}
+        <AuthContext.Provider value={this.state.authenticated}>{persons}</AuthContext.Provider>
       </Aux>
     );
 

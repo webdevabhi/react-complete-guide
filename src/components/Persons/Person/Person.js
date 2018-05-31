2aaddacf723aa886728e79bb6932a049e7784203
webdevabhi <abhishek.patel185@gmail.com>
2018-05-31 10:45:19 +0530

    React Context Api to pass variables globally.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,20 +4,21 @@ import PropTypes from 'prop-types';
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 import Aux from '../../../hoc/Aux';
+import { AuthContext } from '../../../containers/App';
 
 class Person extends Component {
   constructor(props) {
     super(props);
-    // console.log('[Person.js] Inside Constructor', props);
+    console.log('[Person.js] Inside Constructor', props);
     this.inputElement  = React.createRef();
   }
 
   componentWillMount() {
-    // console.log('[Person.js] Inside componentWillMount');
+    console.log('[Person.js] Inside componentWillMount');
   }
 
   componentDidMount() {
-    // console.log('[Person.js] Inside componentDidMount');
+    console.log('[Person.js] Inside componentDidMount');
     if (this.props.position === 0) {
       this.inputElement.current.focus();
     }
@@ -28,10 +29,14 @@ class Person extends Component {
   }
 
   render() {
-    // console.log('[Person.js] Inside render');
+    console.log('[Person.js] Inside render');
     return (
       <Aux>
-        {this.props.authenticated ? <p>I am Authenticated !</p> : null}
+
+        <AuthContext.Consumer>
+        {auth => auth ? <p>I am Authenticated !</p> : null}
+        </AuthContext.Consumer>
+
         <p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old.</p>
         {this.props.children}
 
